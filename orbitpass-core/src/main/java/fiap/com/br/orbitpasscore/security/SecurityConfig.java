ce7c8160a96c863d9ee151744068e706fff2e4c6
Lucas <lucasjlima2006@gmail.com>
2026-06-07 14:45:34 -0300

    fix(security): allow public access to /swagger-ui.html redirect path

--- orbitpass-core/src/main/java/fiap/com/br/orbitpasscore/security/SecurityConfig.java
+++ orbitpass-core/src/main/java/fiap/com/br/orbitpasscore/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                         .authenticationEntryPoint(securityEntryPoints.authenticationEntryPoint())
                         .accessDeniedHandler(securityEntryPoints.accessDeniedHandler()))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/users").hasRole("ADMIN")
